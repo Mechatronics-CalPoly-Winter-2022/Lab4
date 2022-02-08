1b2b72a3e29b568c20f30ca1759ef76daabb011d
kcjennin <kcjennin@calpoly.edu>
2022-02-08 14:14:19 -0800

    setup2

--- src/pc_serial.py
+++ src/pc_serial.py
@@ -1,6 +1,6 @@
 import time
 import serial
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 def main():
@@ -15,7 +15,6 @@ def main():
             print('Invalid input, exiting')
             return
 
-        data: list = [[], [], []]
         ser.write((str(kp) + '\r\n').encode())
         ser.write((str(period) + '\r\n').encode())
         time.sleep(0.1)
@@ -24,39 +23,7 @@ def main():
         ser.readline()
 
         while True:
-            line: str = ser.readline().decode()
-            if line == 'end.\r\n':
-                break
-            if ',' in line:
-                name, meat = line.split(':')[0].strip(), line.split(':')[1].strip()
-                meat = list(map(str.strip, meat.split(',')))
-                data[0].append(name)
-                data[1].append(meat[0])
-                data[2].append(meat[1])
-
-        servo1 = [[], []]
-        servo2 = [[], []]
-        for i in range(len(data[0])):
-            if data[0][i] == 'servo1':
-                servo1[0].append(float(data[1][i]))
-                servo1[1].append(float(data[2][i]))
-            if data[0][i] == 'servo2':
-                servo2[0].append(float(data[1][i]))
-                servo2[1].append(float(data[2][i]))
-
-        figure, axis = plt.subplots(2, 1)
-
-        axis[0].plot(servo1[0], servo1[1])
-        axis[0].set_title('Servo 1')
-        axis[0].set_xlabel('Time (s)')
-        axis[0].set_ylabel('Position (Ticks)')
-
-        axis[1].plot(servo2[0], servo2[1])
-        axis[1].set_title('Servo 2')
-        axis[1].set_xlabel('Time (s)')
-        axis[1].set_ylabel('Position (Ticks)')
-
-        plt.show()
+            pass
 
 
 if __name__ == '__main__':
